79d52880b59f2258822dd7ba90a85a98696370bf
Duarte Costa <duartecosta3558@hotmail.com>
2021-06-10 12:20:59 +0100

    firebase config

--- src/services/FBase.js
+++ src/services/FBase.js
@@ -1,13 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firebase-auth";
 import "firebase/firestore";
-import 'firebase/storage';
+import "firebase/storage";
 
 const fbase = firebase.initializeApp({
-
+  apiKey: "AIzaSyA_Tmo5q4NpR1PvC3XCoMKKELmt3bl166I",
+  authDomain: "davincisfridge.firebaseapp.com",
+  projectId: "davincisfridge",
+  storageBucket: "davincisfridge.appspot.com",
+  messagingSenderId: "233386190298",
+  appId: "1:233386190298:web:0e7bdde82de9f6f7000ce3",
+  measurementId: "G-RRZFL2GT2B",
 });
 
 export default fbase;
-
-
-  
